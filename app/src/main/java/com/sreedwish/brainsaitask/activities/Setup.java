eae4c4d034ea6a8c1ed4d14462d71fc461d48265
sreedwish <sreedwishjk@gmail.com>
2019-12-29 23:51:37 +0530

    commit 2

---
+++ app/src/main/java/com/sreedwish/brainsaitask/activities/Setup.java
@@ -0,0 +1,22 @@
+package com.sreedwish.brainsaitask.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sreedwish.brainsaitask.R;
+import com.sreedwish.brainsaitask.activities.ui.setup.SetupFragment;
+
+public class Setup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.setup_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, SetupFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
---
+++ app/src/main/java/com/sreedwish/brainsaitask/activities/ui/setup/SetupViewModel.java
@@ -0,0 +1,7 @@
+package com.sreedwish.brainsaitask.activities.ui.setup;
+
+import androidx.lifecycle.ViewModel;
+
+public class SetupViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
