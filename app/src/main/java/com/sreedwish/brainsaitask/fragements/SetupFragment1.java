cee06ed999eb8358041e21d356fd3a6476bfb2ed
Sreedwish <2034739+sreedwish@users.noreply.github.com>
2019-12-30 12:26:41 +0530

    fragment layout finished

---
+++ app/src/main/java/com/sreedwish/brainsaitask/fragements/SetupFragment1.java
@@ -0,0 +1,42 @@
+package com.sreedwish.brainsaitask.fragements;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.sreedwish.brainsaitask.R;
+import com.sreedwish.brainsaitask.viewmodel.SetupViewModel;
+
+public class SetupFragment1 extends Fragment {
+
+    private SetupViewModel mViewModel;
+
+    public static SetupFragment1 newInstance() {
+        return new SetupFragment1();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.setup_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(SetupViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+
+
+}
--- app/src/main/java/com/sreedwish/brainsaitask/fragements/SetupFragment2.java
+++ app/src/main/java/com/sreedwish/brainsaitask/fragements/SetupFragment2.java
@@ -1,26 +1,24 @@
 package com.sreedwish.brainsaitask.fragements;
 
-import androidx.lifecycle.ViewModelProviders;
-
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+
 import com.sreedwish.brainsaitask.R;
 import com.sreedwish.brainsaitask.viewmodel.SetupViewModel;
 
-public class SetupFragment extends Fragment {
+public class SetupFragment2 extends Fragment {
 
     private SetupViewModel mViewModel;
 
-    public static SetupFragment newInstance() {
-        return new SetupFragment();
+    public static SetupFragment2 newInstance() {
+        return new SetupFragment2();
     }
 
     @Nullable
@@ -36,5 +34,7 @@ public class SetupFragment extends Fragment {
         mViewModel = ViewModelProviders.of(this).get(SetupViewModel.class);
         // TODO: Use the ViewModel
     }
+
+
 
 }
