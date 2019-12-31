36a3f2ac5e557008c60cd0a3fe6123bb73bf99b6
sreedwish <sreedwishjk@gmail.com>
2019-12-31 09:49:44 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/res/layout/recycle_item.xml
    #	app/src/main/res/values/styles.xml

--- app/src/main/java/com/sreedwish/brainsaitask/activities/Setup.java
+++ app/src/main/java/com/sreedwish/brainsaitask/activities/Setup.java
@@ -1,22 +1,40 @@
 package com.sreedwish.brainsaitask.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
 import com.sreedwish.brainsaitask.R;
+import com.sreedwish.brainsaitask.adapters.ViewPagerAdapter;
+import com.sreedwish.brainsaitask.databinding.SetupActivityBinding;
 import com.sreedwish.brainsaitask.fragements.SetupFragment1;
 
 public class Setup extends AppCompatActivity {
 
+    SetupActivityBinding binding;
+
+    ViewPagerAdapter viewPagerAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.setup_activity);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.container, SetupFragment1.newInstance())
-                    .commitNow();
-        }
+        binding = DataBindingUtil.setContentView(this,R.layout.setup_activity);
+
+        setupViewPager();
+
+    }
+
+    private void setupViewPager() {
+
+        viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+
+        SetupFragment1 fragment1 = SetupFragment1.newInstance();
+
+        viewPagerAdapter.addFragment(fragment1,"frag1");
+
+        binding.viewPager.setAdapter(viewPagerAdapter);
+
+
     }
 }
